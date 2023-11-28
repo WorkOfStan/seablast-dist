ab673b65662ca8233b3655fa7212d47fa7c977dc
WorkOfStan <rejthar@stanislavrejthar.com>
2023-11-28 23:17:39 +0100

    - initial set-up (2023/07/04-11)

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# TODO
+All planned changes to this project will be documented in this file.
+
+## Governance
+- 231128, replace `.github/linters/*.yml` and `.github/workflows/*.yml` by Seablast versions, probably `WorkOfStan/Seablast-github/.github/workflows/`
---
+++ conf/app.conf.php
@@ -0,0 +1,11 @@
+<?php
+
+use Seablast\Seablast\SeablastConfiguration;
+use Seablast\Seablast\SeablastConstant;
+// TODO Description: use AppConstants ... A ty budou nastaveny kde? ... Slouží jako nápověda - každá hodnota je akceptovaná
+
+return static function (SeablastConfiguration $SBConfig): void {
+    $SBConfig->flag
+        ->activate(SeablastConstant::WEB_RUNNING)
+        ;
+};
