eb4497d69d5d6fdb78acd7fdb5d2ce6936858c60
WorkOfStan <rejthar@stanislavrejthar.com>
2025-03-30 22:51:27 +0200

    Update RedirModel.php redirectionUrl isn't deprecated

--- src/Models/RedirModel.php
+++ src/Models/RedirModel.php
@@ -16,10 +16,11 @@ class RedirModel // implements SeablastModelInterface
     public function knowledge(): stdClass
     {
         // typecasting array to stdClass works since PHP4
-        $result = (object) ['httpCode' => 302, 'redirection' => (object) [
-            'url' => // SeablastConstant::SB_APP_ROOT_ABSOLUTE_URL
+        $result = (object) ['httpCode' => 302, 'redirectionUrl' => //(object) [
+            //'url' => // SeablastConstant::SB_APP_ROOT_ABSOLUTE_URL
             './kontakt'
-            ]];
+            //]
+            ];
         return $result;
     }
 }
